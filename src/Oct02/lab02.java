4ec338a9311c8a98c71d4b229bf0f84a8d776b7f
sukkku <sukumar.bollepally@gmail.com>
2024-10-04 15:49:12 +0530

    Initial commit

---
+++ src/Oct02/lab02.java
@@ -0,0 +1,16 @@
+package Oct02;
+
+public class lab02 {
+/*
+*
+* line 1
+* line2
+* line3
+ */
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+
+//       HELLO WROLD IS PRINTED
+        
+    }
+}
---
+++ src/Oct04/CaluculatorProgram.java
@@ -0,0 +1,18 @@
+package Oct04;
+
+public class CaluculatorProgram {
+    public static void main(String[] args) {
+
+        // writing caluculator program using SOP
+        //Addtion
+        System.out.println(8+1);
+        //subtraction
+        System.out.println(8-1);
+        //multiplication
+        System.out.println(8*1);
+        //division
+        System.out.println(8/1);
+
+
+    }
+}
