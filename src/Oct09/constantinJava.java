0c43f04cf219809be9d9a68432eb0c116306ab6b
sukkku <sukumar.bollepally@gmail.com>
2024-10-11 00:31:07 +0530

    Initial commit

--- src/Oct09/constantinJava.java
+++ src/Oct09/constantinJava.java
@@ -9,5 +9,18 @@ public class constantinJava {
         char grade = 'A';
         grade = 'B';
         System.out.println(grade);
+        final float PI = 3.14f;
+         System.out.println(PI);
+       // PI = 10; This is not possible
+
+        //- Special variable whose **value can't be modified during the program execution.**
+        //- One created and SET THE VALUE , we will not able to modify.
+        //- PI - 3.14 , this value should be change during the execution of the program.
+        //- Constant is also called as **final** variable
+        //If you try to change the value - `java: cannot assign a value to final variable PI`
+        //
+        //
+        //
+        //**Rule : Default value of the Local variable is not assigned by the JVM. (Error)**
     }
 }
